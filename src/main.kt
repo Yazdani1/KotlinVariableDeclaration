2dc10d8ee1ba66c39c87046a71df1f6f99005b88
Yazdani <yaz4dani@gmail.com>
2017-07-09 21:31:55 +0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by Yazdani on 7/9/2017.
+ */
+
+fun main(args : Array<String>){
+
+    var a=20
+
+    a=50
+
+    println(a)
+
+    var name:String="Hello It's Yazdani"
+    println(name)
+    println("What's Up "+name)
+    println("What's Up Mr. ${name}")
+
+}
